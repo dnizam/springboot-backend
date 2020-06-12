b6829d500a4f42e91dfe05bbe0fd87862e05866a
dnizam <41112906+dnizam@users.noreply.github.com>
2020-06-12 16:51:08 +0530

    Update gke-cluster.tf

--- terraform/gke-cluster.tf
+++ terraform/gke-cluster.tf
@@ -4,9 +4,13 @@ resource "google_container_cluster" "gke-cluster" {
   initial_node_count = 2
 
   node_config {
+    service_account = "${google_service_account.gke_nodes.email}"
     preemptible  = true
     machine_type = "n1-standard-1"
     oauth_scopes = [
+      "storage-ro",
+      "logging-write",
+      "monitoring",
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
     ]
